9d0864bdf30912374e2864097770dad98892d4b9
Andrew <atrybushnyi@kse.org.ua>
2022-10-27 21:25:12 +0300

    Lib usage logic implementation

---
+++ caesarLib/library.h
@@ -0,0 +1,9 @@
+#ifndef SHAREDCAESAR_LIBRARY_H
+#define SHAREDCAESAR_LIBRARY_H
+
+#include <string>
+std::string Encrypt(std::string a,int key);
+std::string Decrypt(std::string a, int key);
+
+
+#endif //SHAREDCAESAR_LIBRARY_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-
+#include "library.h"
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << Encrypt("help", 3) << std::endl;
     return 0;
 }
