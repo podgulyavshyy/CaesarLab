4186189bc43a8867da780d1662286bd7e68e60ff
podgulyavshyy <mhunko@yahoo.com>
2022-10-27 21:34:41 +0300

    CLI encrypt command added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include "library.h"
+
 int main() {
-    std::cout << Encrypt("help", 3) << std::endl;
-    return 0;
+    int key = 0;
+    while (true) {
+        std::string text;
+        std::cout << "Enter the text: ";
+        std::cin >> text;
+        fflush(stdin);
+        std::string key;
+        std::cout << "Enter key: ";
+        std::cin >> key;
+        fflush(stdin);
+        std::cout << Encrypt(text,std::stoi(key)) << std::endl;
+    }
 }
